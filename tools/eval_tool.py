4ad1fa777bb8fa5b37f261856b29573eba3c1bb0
Iris88 <xiwudamengbi@foxmail.com>
2022-09-21 23:40:00 +0800

    Update eval_tool.py

--- tools/eval_tool.py
+++ tools/eval_tool.py
@@ -120,7 +120,6 @@ def valid(model, dataset, epoch, writer, config, gpu_list, output_function, mode
 
         if step % output_time == 0:
             delta_t = timer() - start_time
-
             output_value(epoch, mode, "%d/%d" % (step + 1, total_len), "%s/%s" % (
                 gen_time_str(delta_t), gen_time_str(delta_t * (total_len - step - 1) / (step + 1))),
                          "%.3lf" % (total_loss / (step + 1)), output_info, '\r', config)
@@ -142,6 +141,68 @@ def valid(model, dataset, epoch, writer, config, gpu_list, output_function, mode
     micro_prec_query, micro_recall_query, micro_f1_query = eval_micro_query(result)
     loss_tmp = total_loss / (step + 1)
     print('valid set: micro_prec_query=%.4f, micro_recall_query=%.4f, micro_f1_query=%.4f' %
+          (micro_prec_query, micro_recall_query, micro_f1_query))
+
+    model.train()
+    return {'precision': micro_prec_query, 'recall': micro_recall_query, 'f1': micro_f1_query, 'loss': loss_tmp}
+
+def test(model, dataset, epoch, writer, config, gpu_list, output_function, mode="test"):
+    model.eval()
+    acc_result = None
+    total_loss = 0
+    cnt = 0
+    total_len = len(dataset)
+    start_time = timer()
+    output_info = ""
+
+    output_time = config.getint("output", "output_time")
+    step = -1
+    more = ""
+    if total_len < 10000:
+        more = "\t"
+    result = []
+
+    for step, data in enumerate(dataset):
+        for key in data.keys():
+            if isinstance(data[key], torch.Tensor):
+                if len(gpu_list) > 0:
+                    data[key] = Variable(data[key].cuda())
+                else:
+                    data[key] = Variable(data[key])
+
+        results = model(data, config, gpu_list, acc_result, "test")
+        # print(results)
+        # acc_result, output = results["acc_result"], results["output"]
+        # loss, acc_result, output = results["loss"], results["acc_result"], results["output"]
+        # total_loss += float(loss)
+        output = results["output"]
+        result += output
+        cnt += 1
+
+        if step % output_time == 0:
+            delta_t = timer() - start_time
+            output_value(epoch, mode, "%d/%d" % (step + 1, total_len), "%s/%s" % (
+                gen_time_str(delta_t), gen_time_str(delta_t * (total_len - step - 1) / (step + 1))),
+                         "%.3lf" % (total_loss / (step + 1)), output_info, '\r', config)
+
+    if step == -1:
+        logger.error("There is no data given to the model in this epoch, check your data.")
+        raise NotImplementedError
+
+    delta_t = timer() - start_time
+    # output_info = output_function(acc_result, config)
+    # output_value(epoch, mode, "%d/%d" % (step + 1, total_len), "%s/%s" % (
+    #     gen_time_str(delta_t), gen_time_str(delta_t * (total_len - step - 1) / (step + 1))),
+    #              "%.3lf" % (total_loss / (step + 1)), output_info, None, config)
+
+    # writer.add_scalar(config.get("output", "model_name") + "_eval_epoch", float(total_loss) / (step + 1),
+    #                   epoch)
+
+    # eval results based on query micro F1
+    print("--------------------------")
+    micro_prec_query, micro_recall_query, micro_f1_query = eval_micro_query(result)
+    loss_tmp = total_loss / (step + 1)
+    print('test set: micro_prec_query=%.4f, micro_recall_query=%.4f, micro_f1_query=%.4f' %
           (micro_prec_query, micro_recall_query, micro_f1_query))
 
     model.train()
